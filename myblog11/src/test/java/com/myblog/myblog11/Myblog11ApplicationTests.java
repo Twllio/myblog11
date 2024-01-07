9d1885f9b97b60bdffca09347b8267cdad616726
Nikhilpawar <nikhilpawar5855@gmail.com>
2024-01-07 19:03:22 +0530

    127: Create save Post Feature Entity Class added complete

--- myblog11/src/test/java/com/myblog/myblog11/Myblog11ApplicationTests.java
+++ myblog11/src/test/java/com/myblog/myblog11/Myblog11ApplicationTests.java
@@ -1,13 +1,14 @@
 package com.myblog.myblog11;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Myblog11ApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog11Application.class, args);
 	}
 
 }
